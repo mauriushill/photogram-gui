bcdc1ccaab175b12101a5fd6b112c6de4a16e160
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2024-02-15 22:16:50 +0000

    updated user and photo routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   get("/", { :controller => "user", :action => "index"})
   get("/users", { :controller => "user", :action => "index"})
   get("/users/:username", { :controller => "user", :action => "show"})
-
+  post("/insert_user", { :controller => "user", :action => "insert"})
+  post("/update_user/:the_user", { :controller => "user", :action => "update"})
+  get("/photos", { :controller => "photo", :action => "list"})
+  post("/insert_photo", { :controller => "photo", :action => "insert"})
+  get("/photos/:a_photo", { :controller => "photo", :action => "show"})
+  post
 end
